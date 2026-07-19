0a5902b7f2d5888493b6fde7dc5e8e54d4ccea02
NeelimaReddy123 <neelimareddy715@gmail.com>
2026-07-19 22:53:37 +0530

    chore: complete framework documentation layout and config file migration

--- utils/read_configurations.py
+++ utils/read_configurations.py
@@ -7,7 +7,7 @@ def read_configuration(category, key):
     config = configparser.ConfigParser()
     # Get the absolute path to the directory containing this script
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(current_dir, "config.ini")
+    config_path = os.path.join(current_dir, "../Configurations/config.ini")
 
     config.read(config_path)
     return config.get(category, key)
